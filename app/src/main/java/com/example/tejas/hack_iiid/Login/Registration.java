826dd499d62c93facfeb3817773b244a8913dff1
Radhika Bansal <radhikabansal5898@hotmail.com>
2018-08-17 17:40:19 +0530

    'splashchange'

--- app/src/main/java/com/example/tejas/hack_iiid/Login/Registration.java
+++ app/src/main/java/com/example/tejas/hack_iiid/Login/Registration.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.tejas.hack_iiid.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
     }
 }
