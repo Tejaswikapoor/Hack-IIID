c3c70f7ce2cd8f98b439c6b463399b293dc142a7
Radhika Bansal <radhikabansal5898@hotmail.com>
2018-08-17 21:30:52 +0530

    card

--- app/src/main/java/com/example/tejas/hack_iiid/Splash/Splash.java
+++ app/src/main/java/com/example/tejas/hack_iiid/Splash/Splash.java
@@ -5,11 +5,9 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-<<<<<<< HEAD
+
 import com.airbnb.lottie.LottieAnimationView;
-=======
 import com.example.tejas.hack_iiid.HOME.home_act;
->>>>>>> 0a1b11f533726ab557d414ee089cba18dafa5087
 import com.example.tejas.hack_iiid.MainActivity;
 import com.example.tejas.hack_iiid.R;
 
@@ -30,13 +28,11 @@ public class Splash extends AppCompatActivity {
             @Override
             public void run()
             {
-<<<<<<< HEAD
                 Intent i = new Intent(Splash.this, home_act.class);
                 startActivity(i);
-=======
                 Intent intent = new Intent(Splash.this, MainActivity.class);
                 startActivity(intent);
->>>>>>> 77df49f9dcf1124bf0e8b3f5cf0e71a7f3040395
+
                 finish();
             }
         }, SPLASH_TIME_OUT);
