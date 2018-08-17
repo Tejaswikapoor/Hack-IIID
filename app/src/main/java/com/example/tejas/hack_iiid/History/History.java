aa54b26b54e86e124bf49850c7564acdc7a93b88
Vasudev <vasudev.w13@gmail.com>
2018-08-18 03:23:53 +0530

    meal

---
+++ app/src/main/java/com/example/tejas/hack_iiid/History/History.java
@@ -0,0 +1,15 @@
+package com.example.tejas.hack_iiid.History;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tejas.hack_iiid.R;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
