5452647311afe98028c20faa15d9702a7f97ece6
Vasudev <vasudev.w13@gmail.com>
2018-08-17 17:00:24 +0530

    splash added

--- app/src/main/java/com/example/tejas/hack_iiid/MainActivity.java
+++ app/src/main/java/com/example/tejas/hack_iiid/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.tejas.hack_iiid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LottieAnimationView splashAnim=findViewById(R.id.splashAnimView);
+        splashAnim.playAnimation();
     }
 }
